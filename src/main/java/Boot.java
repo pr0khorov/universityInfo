4736500e59e3f91ec317749c1b2915de3303898d
pr0khorov <prokhorov.d.o@gmail.com>
2021-04-10 14:02:52 +0300

    formatted

--- src/main/java/Boot.java
+++ src/main/java/Boot.java
@@ -1,10 +1,13 @@
 import model.Student;
 
 public class Boot {
+
     public static void main(String[] args) {
-        System.out.println("It works!");
         Student student = new Student();
-        student.setAvgExamScore((float)3.5).setCurrentCourseNumber(3).setFullName("azaza").setUniversityId("123asd");
+        student.setAvgExamScore((float)3.5)
+                .setCurrentCourseNumber(3)
+                .setFullName("azaza")
+                .setUniversityId("123asd");
         System.out.println(student);
     }
 }
